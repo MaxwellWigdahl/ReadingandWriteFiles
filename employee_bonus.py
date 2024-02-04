9535d73a02f4fd2d1dcc383c27472bea7814fad5
Max <max_wigdahl1@baylor.edu>
2024-02-04 14:20:18 -0600

    update

--- employee_bonus.py
+++ employee_bonus.py
@@ -2,15 +2,11 @@ import csv
 
 def main():
     infile_name = r'C:\Users\maxww\Documents\AdvPython\readandwritefiles\employee_data.csv'
-
-    # Explicitly open the file
     infile = open(infile_name, 'r', newline='')
 
-    # CSV reader
     reader = csv.reader(infile)
     fields = next(reader)
 
-    # Process data
     for row in reader:
         employee_ID = row[0]
         name = row[1]
@@ -21,12 +17,12 @@ def main():
         team = row[6]
         bonus = float(row[7])
 
-        bonus_amt = bonus * salary
-        total_salary = bonus_amt + salary
+        bonus_amts = bonus * salary
+        total_salary = bonus_amts + salary
 
         print(f"Name: {name}")
         print(f"Salary: $ {salary:,.2f}")
-        print(f"Bonus:  $  {bonus_amt:,.2f}")
+        print(f"Bonus:  $  {bonus_amts:,.2f}")
         print(f"Pay:    $ {total_salary:,.2f}")
         print()
 
