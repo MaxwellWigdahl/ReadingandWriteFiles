6b3da0a3ec322e9c02bf86ba047c1172234f5c92
Max <max_wigdahl1@baylor.edu>
2024-02-04 14:14:27 -0600

    update

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,29 +1,35 @@
 import csv
 
 def main():
-    infile_name = 'employee_data.csv'
+    infile_name = r'C:\Users\maxww\Documents\AdvPython\readandwritefiles\employee_data.csv'
 
-    with open(infile_name, 'r', newline='') as infile:
-        reader = csv.reader(infile)
-        fields = next(reader)
+    # Explicitly open the file
+    infile = open(infile_name, 'r', newline='')
 
-        for row in reader:
-            employee_ID = row[0]
-            name = row[1]
-            age = row[2]
-            salary = float(row[3])
-            hours_worked = int(row[4])
-            productivity = row[5]
-            team = row[6]
-            bonus = float(row[7])
+    # CSV reader
+    reader = csv.reader(infile)
+    fields = next(reader)
 
-            bonus_amt = bonus * salary
-            total_salary = bonus_amt + salary
+    # Process data
+    for row in reader:
+        employee_ID = row[0]
+        name = row[1]
+        age = row[2]
+        salary = float(row[3])
+        hours_worked = int(row[4])
+        productivity = row[5]
+        team = row[6]
+        bonus = float(row[7])
 
-            print(f"Name: {name}")
-            print(f"Salary: $ {salary:,.2f}")
-            print(f"Bonus:  $  {bonus_amt:,.2f}")
-            print(f"Pay:    $ {total_salary:,.2f}")
-            print()  # Add an empty line between each employee's information
+        bonus_amt = bonus * salary
+        total_salary = bonus_amt + salary
+
+        print(f"Name: {name}")
+        print(f"Salary: $ {salary:,.2f}")
+        print(f"Bonus:  $  {bonus_amt:,.2f}")
+        print(f"Pay:    $ {total_salary:,.2f}")
+        print()
+
+    infile.close()
 
 main()
